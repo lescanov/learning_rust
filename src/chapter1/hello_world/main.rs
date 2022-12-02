8f9e4f1de38ce2c6082755b67a99d57622a2ca42
Leo Escano <lescano.v.jr@gmail.com>
2022-12-02 08:38:54 -0800

    wrote hello_world program in rust

---
+++ src/chapter1/hello_world/main.rs
@@ -0,0 +1,9 @@
+// Anatomy of rust code:
+// The main function is the first code that runs in each rust program
+// Curly braces encapsulates function code
+// ! operator is used to call a macro
+// ; is used to end an expression
+
+fn main() {
+    println!("Hello, world!");
+}
